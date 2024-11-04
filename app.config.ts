e96f5fd579814682cf04daf6827d218c146f508f
notKamui <teillard.jimmy@outlook.fr>
2024-11-04 22:40:19 +0100

    fixed trusted deps

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,15 @@
+import path from 'node:path'
 import { defineConfig } from '@tanstack/start/config'
 
 export default defineConfig({
   server: {
     preset: 'bun',
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'app'),
+      },
+    },
   },
 })
